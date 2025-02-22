2c2df5ffd4e29d7279b38873fe5c988ee9821fa2
eunsang <thecynicdog0328@gmail.com>
2025-02-22 20:35:22 +0900

    chore:

--- src/main/java/kr/co/metlife/pseudomgtsystemapi/store/repository/ConfigTableColumnRepository.java
+++ src/main/java/kr/co/metlife/pseudomgtsystemapi/store/repository/ConfigTableColumnRepository.java
@@ -2,7 +2,9 @@ package kr.co.metlife.pseudomgtsystemapi.store.repository;
 
 import kr.co.metlife.pseudomgtsystemapi.store.entity.ConfigTableColumn;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConfigTableColumnRepository extends JpaRepository<ConfigTableColumn, Long> {
 
 }
--- src/main/java/kr/co/metlife/pseudomgtsystemapi/store/repository/ConfigTableRepository.java
+++ src/main/java/kr/co/metlife/pseudomgtsystemapi/store/repository/ConfigTableRepository.java
@@ -2,7 +2,9 @@ package kr.co.metlife.pseudomgtsystemapi.store.repository;
 
 import kr.co.metlife.pseudomgtsystemapi.store.entity.ConfigTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConfigTableRepository extends JpaRepository<ConfigTable, Long> {
 
     ConfigTable findByProjectNameAndName(String project, String table);
