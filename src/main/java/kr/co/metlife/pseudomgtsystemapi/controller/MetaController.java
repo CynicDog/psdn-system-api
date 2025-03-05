d512d3ba6f60665ce0b97dd33090b0a73f80c11b
eunsang <thecynicdog0328@gmail.com>
2025-03-05 16:13:04 +0900

    chore:

---
+++ src/main/java/kr/co/metlife/pseudomgtsystemapi/controller/MetaController.java
@@ -0,0 +1,9 @@
+package kr.co.metlife.pseudomgtsystemapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MetaController {
+}
